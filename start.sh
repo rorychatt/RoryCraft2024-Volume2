18ff541f7012b86cfa1c3d075dad54315523b555
rorychatt <pavel.rinne@gmail.com>
2024-07-13 19:01:24 +0200

    Feat: start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+MAX_RAM="10000M"
+MIN_RAM="4000M"
+
+JVM_ARGS="-XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC"
+SERVER_JAR="paper.jar"
+SERVER_ARGS=""
+
+while true; do
+    echo "Waiting for 5 seconds before starting the server..."
+    sleep 5  # Wait for 5 seconds
+    java -Xmx$MAX_RAM -Xms$MIN_RAM $JVM_ARGS -jar $SERVER_JAR $SERVER_ARGS
+    echo "Server process crashed. Restarting..."
+    sleep 1
+done
